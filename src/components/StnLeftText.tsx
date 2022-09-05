5a9582650f441d1ede9ac60b753c1c273bd9ddcc
tinycodecraft <raymond.cw.yau@outlook.com>
2022-09-05 16:21:37 +0800

    add framer motion (test)

--- src/components/StnLeftText.tsx
+++ src/components/StnLeftText.tsx
@@ -1,15 +1,14 @@
-import {Text,   useMantineTheme} from '@mantine/core';
-import {useMediaQuery} from '@mantine/hooks';
-import {graphql, useStaticQuery} from 'gatsby';
-import React, {Fragment} from 'react'
-import HeadingPcs from './Elements/Pieces/HeadingPcs';
-import TextPcs from './Elements/Pieces/TextPcs';
-import NewHeart from './NewHeart';
-
-import StnHeart from './StnHeart';
+import { Text, useMantineTheme } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import { graphql, useStaticQuery } from 'gatsby'
+import React, { Fragment } from 'react'
+import HeadingPcs from './Elements/Pieces/HeadingPcs'
+import TextPcs from './Elements/Pieces/TextPcs'
+import NewHeart from './NewHeart'
+import { motion } from 'framer-motion'
 
 // Query Part
-const qyEvent = graphql `
+const qyEvent = graphql`
   query {
     event {
       profiles {
@@ -25,44 +24,78 @@ const qyEvent = graphql `
       }
     }
   }
-`;
+`
 // Fragment
-
-
 
 // Function Part
 export default function StnLeftText() {
-    const desktop = useMediaQuery(`(min-width: ${
-        768 / 16
-    }em`);
-    const {event} = useStaticQuery(qyEvent);
-    const theme = useMantineTheme();
-    const mode = desktop ? 'desktop': 'phone';
-    return (
-        <Fragment>
-            <HeadingPcs mode={mode}
-                size='h1'>
-                <NewHeart></NewHeart>
-                <TextPcs cp='span' toBig={true} mode={mode}>
-                    {
-                    event.profiles[0].name
-                }</TextPcs>
-                <TextPcs cp='span' toBig={true}>&</TextPcs>
-                <TextPcs cp='span' toBig={true}  mode={mode}>
-                    {
-                    event.profiles[1].name
-                }</TextPcs>
-            </HeadingPcs>
-            <TextPcs cp='p'>Join us on</TextPcs>
+  const desktop = useMediaQuery(`(min-width: ${768 / 16}em`)
+  const { event } = useStaticQuery(qyEvent)
+  const theme = useMantineTheme()
+  const mode = desktop ? 'desktop' : 'phone'
+  return (
+    <Fragment>
+      <HeadingPcs mode={mode} size='h1'>
+        <NewHeart></NewHeart>
+        <TextPcs cp='span' toBig={true} mode={mode}>
+          {event.profiles[0].name}
+        </TextPcs>
+        <TextPcs cp='span' toBig={true}>
+          &
+        </TextPcs>
+        <TextPcs cp='span' toBig={true} mode={mode}>
+          {event.profiles[1].name}
+        </TextPcs>
+      </HeadingPcs>
+      <TextPcs cp='p'>Join us on</TextPcs>
 
-            <Text color={theme.colors.violet[2]}
-                weight={desktop ? 600 : 400} size={desktop ? 'lg': 'xl'}>
-                {event.events[0].title}</Text>
-                <TextPcs cp='span' toBig={true}>at</TextPcs>
-            <Text color={theme.colors.violet[2]}
-                weight={desktop ? 600 : 400} size={desktop ? 'lg': 'xl'}>
-                {event.events[0].occasion.place.name}</Text>
-        </Fragment>
+      <motion.div initial={{ opacity: 0, scale: 2 }} 
+      animate={{
+        opacity: 1,
+        scale: 1
+      
+      }}
+      transition={{
+        duration: 1,
+        delay: 0.2
+      }}
+      >
+        <Text
+          color={theme.colors.violet[2]}
+          weight={desktop ? 600 : 400}
+          size={desktop ? 'lg' : 'xl'}
+        >
+          {event.events[0].title}
+        </Text>
+      </motion.div>
 
-    )
+      <TextPcs cp='span' toBig={true}>
+        at
+      </TextPcs>
+      <motion.div initial={{
+        opacity: 0, scale:2,
+        rotateY: -100,
+        
+      }}
+      animate={{
+        opacity: 1, scale:1,
+        rotateY: 0,
+      
+      }}
+      transition={{
+        duration: 1,
+        delay:0.7
+      }}
+      >
+      <Text
+        color={theme.colors.violet[2]}
+        weight={desktop ? 600 : 400}
+        size={desktop ? 'lg' : 'xl'}
+      >
+        {event.events[0].occasion.place.name}
+      </Text>
+      </motion.div>
+
+    </Fragment>
+  )
 }
